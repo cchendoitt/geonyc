860b0aaccfd86acc6bd0792c6bbba89f7d8e24b9
cchendoitt <39533822+cchendoitt@users.noreply.github.com>
2022-07-01 13:12:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 import json
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
    print('Request for index page received')
@@ -12,14 +13,13 @@ def index():
 def geocoding():
    location = request.form.get('name')
 
-   if len(location.strip()) == 0:
-       return render_template('index.html', location = 'No location entered!')
+   #if len(location.strip()) == 0:
+   #    return render_template('index.html', location = 'No location entered!')
 
    from bigapple.parser import BigAppleParser
    p = BigAppleParser()
-   #parsed = p.parse(location.upper())
    parsed = p.parse(location)
-   standardized_address = parsed['components']['output_address']
+   #standardized_address = parsed['components']['output_address']
    
    json_object = json.dumps(parsed) 
    #return render_template('index.html', location = location, name = json_object)
